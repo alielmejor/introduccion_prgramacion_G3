946c08e93c45d7ad8e597307d6ebd0385adefa4f
alielmejor <aazeledon@uamv.edu.ni>
2026-05-13 10:41:52 -0600

    Add files via upload

---
+++ ejercicio1,13demayo2026.cpp
@@ -0,0 +1,23 @@
+    Matriz 3 x 3
+    int [,] matriz = new int [3, 3];
+
+    Console.Writeline( dijite los elementos de la matriz 3x3:");
+    for (int f = 0; f < 3; f++)
+    {
+    for (int c = 0; c < 3; c++)
+         {
+            console.Writeline("$Elemento [(f),{c}: ")
+            matriz [f , c] = int.Parse(console:Readline());
+            }
+ {
+
+ Console.Writeline("Mostrar matriz");
+ for (int c = 0; c < 3; c++)
+{
+             for (int c = 0, c < 3; c++)
+             {
+                  Console.Write(${matriz[f , c]} ");
+
+             }    
+             Console.Writeline();
+}
